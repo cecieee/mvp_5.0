1833b14a0ac46e95f3fa3b6896b00469d816103c
NivedGanga <nivedganga@gmail.com>
2023-08-05 17:41:02 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+  function loadScoresGSheet() {
+
+    let sheetUrl = "https://docs.google.com/spreadsheets/d/1IBrHZ5zt5EP9TILd-YTCbHVipz6A8dnXheZsF_VcmVc/edit#gid=0";
+    $('#rank').sheetrock({
+      url: sheetUrl,
+      query: `SELECT A, B WHERE B >= 20 order by B desc limit 3`,
+      labels: ["\u0000", ""],
+      callback: () => {},
+    });
+  };
+  
+  
+  $(document).ready(loadScoresGSheet);
