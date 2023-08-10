8725b2861de67059353fe1e733b36ba014ffef9e
NivedGanga <nivedganga@gmail.com>
2023-08-10 07:17:31 +0530

    made leaderboard reposive

--- js/index.js
+++ js/index.js
@@ -15,10 +15,20 @@
 
   let prizeTitle = document.getElementById("prizes-title");
   let prize =document.getElementById("prizes");
+  let navbar_ = document.getElementById("my-nav");
   window.addEventListener("scroll", function() {
     let value = window.scrollY;
+    console.log(value);
      if (value<3153) {
       prizeTitle.style.fontSize = value *0.008  +"vw";
      }
+
+     if (value>812) {
+      navbar_.style.backgroundColor = "rgb(16, 16, 27)";
+     }
+     else{
+      navbar_.style.backgroundColor ="transparent";
+     }
+     
   });
 
