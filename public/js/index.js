478ef70fb5390ff61efc0339a88a158eaac5152e
NivedGanga <nivedganga@gmail.com>
2023-08-12 14:02:57 +0530

    htacess added

---
+++ public/js/index.js
@@ -0,0 +1,34 @@
+
+  function loadScoresGSheet() {
+
+    let sheetUrl = "https://docs.google.com/spreadsheets/d/1IBrHZ5zt5EP9TILd-YTCbHVipz6A8dnXheZsF_VcmVc/edit#gid=0";
+    $('#rank').sheetrock({
+      url: sheetUrl,
+      query: `SELECT A, B WHERE B >= 20 order by B desc limit 5`,
+      labels: ["\u0000", ""],
+      callback: () => {},
+    });
+  };
+  
+  
+  $(document).ready(loadScoresGSheet);
+
+  let prizeTitle = document.getElementById("prizes-title");
+  let prize =document.getElementById("prizes");
+  let navbar_ = document.getElementById("my-nav");
+  window.addEventListener("scroll", function() {
+    let value = window.scrollY;
+    console.log(value);
+     if (value<3153) {
+      prizeTitle.style.fontSize = value *0.008  +"vw";
+     }
+
+     if (value>812) {
+      navbar_.style.backgroundColor = "rgb(16, 16, 27)";
+     }
+     else{
+      navbar_.style.backgroundColor ="transparent";
+     }
+     
+  });
+
