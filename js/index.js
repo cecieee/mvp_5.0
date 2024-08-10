8a3c84ab9802b7d3a528733d805c5af207615bf2
SHIJIN ABRAHAM <shijinabraham2003@gmail.com>
2024-08-10 21:45:00 +0530

    Resolved Error

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 function loadScoresGSheet() {
-  let sheetUrl = "https://docs.google.com/spreadsheets/d/1IBrHZ5zt5EP9TILd-YTCbHVipz6A8dnXheZsF_VcmVc/edit#gid=0";
-  $('#rank').sheetrock({
-      url: sheetUrl,
-      query: `SELECT A, B WHERE B >= 20 order by B desc limit 5`,
-      labels: ["\u0000", ""],
-      callback: () => {},
+  let sheetUrl =
+    "https://docs.google.com/spreadsheets/d/1IBrHZ5zt5EP9TILd-YTCbHVipz6A8dnXheZsF_VcmVc/edit#gid=0";
+  $("#rank").sheetrock({
+    url: sheetUrl,
+    query: `SELECT A, B WHERE B >= 20 order by B desc limit 5`,
+    labels: ["\u0000", ""],
+    callback: () => {},
   });
-};
+}
 
 $(document).ready(loadScoresGSheet);
 
@@ -15,7 +16,7 @@ let prize = document.getElementById("prizes");
 const navbar_ = document.getElementById("my-nav");
 const progressBar = document.getElementById("progress-bar");
 
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
   let value = window.scrollY;
   let totalHeight = document.documentElement.scrollHeight - window.innerHeight;
   let threshold = totalHeight * 0.2;
@@ -24,20 +25,21 @@ window.addEventListener("scroll", function() {
   console.log(value);
 
   if (prizeTitle && value < 3153) {
-      prizeTitle.style.fontSize = value * 0.008 + "vw";
+    prizeTitle.style.fontSize = value * 0.008 + "vw";
   }
 
   if (navbar_) {
     if (value > 812) {
-        navbar_.style.backgroundColor = "rgb(16, 16, 27)";
+      navbar_.style.backgroundColor = "rgb(16, 16, 27)";
     } else {
-        navbar_.style.backgroundColor = "transparent";
+      navbar_.style.backgroundColor = "transparent";
     }
-
-    if (value < threshold) {
+    if (window.location.pathname != "/tasks.html" && window.location.pathname != "/contact") {
+      if (value < threshold) {
         navbar_.style.opacity = 0;
-    } else {
+      } else {
         navbar_.style.opacity = 100;
+      }
     }
   }
 
